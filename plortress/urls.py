db0a1c3c3802b2c14fc52b52eea32551b977e870
unknown <warsawmecca300@gmail.com>
2023-04-10 18:07:16 +0900

    added main page

--- plortress/urls.py
+++ plortress/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path('', views.main),
     path('admin/', admin.site.urls),
     path('gim/', include('gim.urls'))
 ]
