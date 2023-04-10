0b0d4f8d6fd9af68cc456991e5cfb20ed2b0b13a
unknown <warsawmecca300@gmail.com>
2023-04-10 11:22:06 +0900

    typo

--- plortress/definitions.py
+++ plortress/definitions.py
@@ -1,4 +1,4 @@
 from os.path import dirname, abspath, join
 
 ROOT_DIR = dirname(dirname(abspath(__file__)))
-GEN_PATH = join(ROOT_DIR, '../generator_static')
+GEN_PATH = join(ROOT_DIR, 'generator_static')
