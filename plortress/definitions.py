f713fdcf43e16814df7cd0221425c7f13b1092ca
unknown <warsawmecca300@gmail.com>
2023-04-10 11:01:07 +0900

    changed rel path to abs path

--- plortress/definitions.py
+++ plortress/definitions.py
@@ -1,4 +1,4 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 GEN_PATH = os.path.join(ROOT_DIR, '../generator_static')
